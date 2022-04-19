fb8ef9081adcca9a41a760b4c868d90da1f7dc33
jsavko <james@geekwebservices.com>
2022-04-18 17:32:28 -0700

    chat-message template

--- chat-timestamp.js
+++ chat-timestamp.js
@@ -2,7 +2,16 @@ Hooks.once("init", async () => {
     let chatTimeStamp =  (timestamp) => { 
         return new Date(timestamp).toLocaleTimeString();
     }
-    
-    Handlebars.registerHelper({timeSince: chatTimeStamp});
+    Handlebars.registerHelper({chatTimeStamp: chatTimeStamp});
 
+    CONFIG.ChatMessage =  {
+        documentClass: ChatMessage,
+        collection: Messages,
+        template: "modules/chat-timestamp/templates/sidebar/chat-message.html",
+        sidebarIcon: "fas fa-comments",
+        batchSize: 100
+      }
 });
+
+
+
