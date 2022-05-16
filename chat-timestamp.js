4c9fab2f252fd3c143d524e2cce088133e728d56
jsavko <james@geekwebservices.com>
2022-05-16 11:26:37 -0700

    Fixed issue with chat cards in PF2E system

--- chat-timestamp.js
+++ chat-timestamp.js
@@ -2,16 +2,9 @@ Hooks.once("init", async () => {
     let chatTimeStamp =  (timestamp) => { 
         return new Date(timestamp).toLocaleTimeString();
     }
+
     Handlebars.registerHelper({chatTimeStamp: chatTimeStamp});
 
-    CONFIG.ChatMessage =  {
-        documentClass: ChatMessage,
-        collection: Messages,
-        template: "modules/chat-timestamp/templates/sidebar/chat-message.html",
-        sidebarIcon: "fas fa-comments",
-        batchSize: 100
-      }
+    CONFIG.ChatMessage.template =  "modules/chat-timestamp/templates/sidebar/chat-message.html";
+    
 });
-
-
-
