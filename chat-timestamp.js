c0f84114dea2a5007a3f48f2c1b261250213aec9
SesuUisu <sesu@outlook.com>
2022-06-11 14:57:08 +0200

    Showing Date
    Added the option to show the date of a chat message.
    Added module settings (client):
    - never showing date
    - always showing date
    - showing date on hover
    - FoundryVTT standard

--- chat-timestamp.js
+++ chat-timestamp.js
@@ -1,10 +1,45 @@
+// Settings
+Hooks.on("init", () => {
+    game.settings.register('chat-timestamp', 'showDate', {
+        name: game.i18n.localize("CHAT_TIMESTAMP.showDate.name"),
+        hint: game.i18n.localize("CHAT_TIMESTAMP.showDate.hint"),
+        scope: 'client',
+        config: true,
+        default: 'hover',
+        type: String,
+        choices: {
+            "foundry": game.i18n.localize("CHAT_TIMESTAMP.showDate.foundry"),
+            "never": game.i18n.localize("CHAT_TIMESTAMP.showDate.never"),
+            "hover": game.i18n.localize("CHAT_TIMESTAMP.showDate.hover"),
+            "always": game.i18n.localize("CHAT_TIMESTAMP.showDate.always")
+        },
+        onchange: () => ui.players.render()
+    });
+});
+
+// Timestamp 
 Hooks.once("init", async () => {
-    let chatTimeStamp =  (timestamp) => { 
+    let chatTimeStamp = (timestamp) => { 
         return new Date(timestamp).toLocaleTimeString();
+    }
+    let chatDateStamp = (timestamp) => { 
+        return new Date(timestamp).toLocaleDateString();
     }
 
     Handlebars.registerHelper({chatTimeStamp: chatTimeStamp});
+    Handlebars.registerHelper({chatDateStamp: chatDateStamp});
 
-    CONFIG.ChatMessage.template =  "modules/chat-timestamp/templates/sidebar/chat-message.html";
-    
+    switch(game.settings.get('chat-timestamp', 'showDate')) {
+        case "never": 
+             CONFIG.ChatMessage.template =  "modules/chat-timestamp/templates/sidebar/chat-message.html";
+             break;
+        case "hover": 
+             CONFIG.ChatMessage.template =  "modules/chat-timestamp/templates/sidebar/chat-message-hover.html";
+             break;
+        case "always": 
+             CONFIG.ChatMessage.template =  "modules/chat-timestamp/templates/sidebar/chat-message-date.html";
+             break;
+        case "foundry":
+             break;
+    }
 });
